e61ad67bd81b54c6c0eec7e86c66a7d9e6e3be5a
joaoraf <joaoraf@gmail.com>
2021-06-07 10:51:03 -0300

    Acréscimo de parâmetros pedidos pelo Hazelcast

--- src/main/bash/lexml-parser.sh
+++ src/main/bash/lexml-parser.sh
@@ -4,4 +4,13 @@ CLASSPATH=`find /lexml-parser-pl -name \*.jar -print0 | tr '\000' ':'`
 export CLASSPATH
 export LANG=C.UTF-8
 export LC_ALL=C.UTF_8
-java br.gov.lexml.parser.pl.ws.Main "$@"
+java \ 
+	--add-modules java.se \
+	--add-exports java.base/jdk.internal.ref=ALL-UNNAMED \
+	--add-opens java.base/java.lang=ALL-UNNAMED \
+        --add-opens java.base/java.nio=ALL-UNNAMED \
+        --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+        --add-opens java.management/sun.management=ALL-UNNAMED \
+        --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
+	br.gov.lexml.parser.pl.ws.Main \
+	"$@"
