405eb46bb1f83e9846bd100e7303a0c62259e7a1
joaoraf <joaoraf@gmail.com>
2021-06-29 21:28:39 -0300

    remoção de opções ligadas ao sistema de modulos do java

--- src/main/bash/lexml-parser.sh
+++ src/main/bash/lexml-parser.sh
@@ -4,13 +4,4 @@ CLASSPATH=`find /lexml-parser-pl -name \*.jar -print0 | tr '\000' ':'`
 export CLASSPATH
 export LANG=C.UTF-8
 export LC_ALL=C.UTF_8
-java \ 
-	--add-modules java.se \
-	--add-exports java.base/jdk.internal.ref=ALL-UNNAMED \
-	--add-opens java.base/java.lang=ALL-UNNAMED \
-        --add-opens java.base/java.nio=ALL-UNNAMED \
-        --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
-        --add-opens java.management/sun.management=ALL-UNNAMED \
-        --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
-	br.gov.lexml.parser.pl.ws.Main \
-	"$@"
+java br.gov.lexml.parser.pl.ws.Main "$@"
